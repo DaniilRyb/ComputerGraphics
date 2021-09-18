e49224c7ac342b34b98759e6d307bcd0ff396fd1
Daniil Rybakov <61760429+DaniilRyb@users.noreply.github.com>
2021-09-18 11:53:57 +0300

    added Bresenham's circle

--- 3/script.js
+++ 3/script.js
@@ -52,13 +52,14 @@ function AnimationClockFunc(x0, y0, radius) {
 
 function BresenhamCircle(xStart, yStart, raduisCircle) {
 	ctx.fillStyle = "#ff0000";
-	var x = xStart;
-	var y = yStart;
+	let x = xStart;
+	let y = yStart;
 	xStart = 0;
 	yStart = raduisCircle;
-	var delta = (2 - 2 * raduisCircle);
-	var beta = 0;
+	let delta = (2 - 2 * raduisCircle);
+	let beta = 0;
 	while (yStart >= 0) {
+		// рисуем дуги для всех четвертей
 		ctx.fillRect(x + xStart, y + yStart, 1, 1);
 		ctx.fillRect(x + xStart, y - yStart, 1, 1);
 		ctx.fillRect(x - xStart, y + yStart, 1, 1);
@@ -67,21 +68,29 @@ function BresenhamCircle(xStart, yStart, raduisCircle) {
 			beta = 2 * (delta + yStart) - 1;
 			if (beta <= 0) {
 				xStart++;
-				delta = delta + (2 * xStart + 1);
+				delta += (2 * xStart + 1);
 				continue;
 			}
+			if (beta > 0) {
+				xStart++;
+				yStart--;
+				delta += 2 * xStart - 2 * yStart + 2;
+			}
 		}
-		beta = 2 * (delta - xStart) - 1;
-		if (delta > 0) {
+		if (delta > 0 || delta === 0) {
+			beta = 2 * (delta - xStart) - 1;
+			if (beta <= 0) {
+				xStart++;
+				yStart--;
+				delta += 2 * xStart - 2 * yStart + 2;
+				continue;
+			}
 			if (beta > 0) {
 				yStart--;
-				delta = delta + (1 - 2 * yStart);
-				continue;
+				delta += (1 - 2 * yStart);
+
 			}
 		}
-		xStart++;
-		delta = delta + (2 * xStart - 2 * yStart);
-		yStart--;
 
 
 	}
@@ -90,8 +99,8 @@ function BresenhamCircle(xStart, yStart, raduisCircle) {
 
 var canvas = document.getElementById('sem3');
 var ctx = canvas.getContext('2d');
-var x_start = 220, y_start = 220;
-var R = 200;
+let x_start = 220, y_start = 220;
+let R = 200;
 BresenhamCircle(x_start, y_start, R);
 setInterval(function () {
 	AnimationClockFunc(x_start, y_start, R);
